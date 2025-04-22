aae140709f1af9c970be712a90ec4874c6a4e33e
prem <bendekarprem99@gmail.com>
2025-04-22 23:35:05 +0530

    added command line runner in the main class

--- src/main/java/com/example/rabbitmqdemo/RabbitmqdemoApplication.java
+++ src/main/java/com/example/rabbitmqdemo/RabbitmqdemoApplication.java
@@ -1,13 +1,18 @@
 package com.example.rabbitmqdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RabbitmqdemoApplication {
+public class RabbitmqdemoApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RabbitmqdemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(RabbitmqdemoApplication.class, args);
+  }
 
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println("run Method called");
+  }
 }
